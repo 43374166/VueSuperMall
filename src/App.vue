50c319b0d3d5cf790ccb0e03d7fd63741cb00f44
43374166 <74867220+43374166@users.noreply.github.com>
2021-04-08 23:03:56 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <template>
   <div id="app">
     <keep-alive exclude="Detail">
-      <!-- <router-view></router-view> -->
+      <router-view></router-view>
     </keep-alive>
     <main-tab-bar></main-tab-bar>
-    <api />
+    <!-- <api /> -->
   </div>
 </template>
 
 <script>
 import MainTabBar from 'components/content/mainTabBar/MainTabBar.vue'
-import Api from './api/api.vue'
+// import Api from './api/api.vue'
 
 
 export default {
   components: {
     MainTabBar,
-    Api
+    // Api
   }
 }
 </script>
